b90aa4421356b6dd760915df498e519e8a3cf608
marcotukan <126711423+marcotukan@users.noreply.github.com>
2023-04-27 02:22:34 -0600

    Comments on bash

--- pdf_scrapper.sh
+++ pdf_scrapper.sh
@@ -7,10 +7,10 @@ curl $url > file.pdf
 # Extract
 pdftotext -layout file.pdf output_file.txt
 
-# Preprocess
+# Preprocess (just adding commas, and separating words from numbers)
 grep -v -e '^$' output_file.txt | sed 's/,//g' | sed 's/^/  /'| sed -E 's/  /,/g'| tr -s ',' | sed s/,// > preproces.txt
 
-# Filter
+# Filter (we are just keeping the lines that have 3 columns: 1 string and 2 numeric)
 grep -v -e '^$' preproces.txt | grep -P '[0-9]+$'|grep -P '., ?[0-9]+, ?[0-9]+'  > data.csv
 
 
