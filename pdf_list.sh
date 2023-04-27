f31731c2befeac34c7942d8e9c5ff6a5024e95c4
marcotukan <126711423+marcotukan@users.noreply.github.com>
2023-04-27 01:55:40 -0600

    Create pdf_list template

---
+++ pdf_list.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+url=$"https://divulgacion.covaf.com/download.php?mode=5&ruta=T1BNVUxUSVwyMDIzXE1BUlpPXE1VTFRJQVJcQ0FSVEVSQVNfTUVOU1VBTEVTXA==&file=Q2FydGVyYU1lbnN1YWxfTVVMVElBUl8wMzIwMjMucGRm"
